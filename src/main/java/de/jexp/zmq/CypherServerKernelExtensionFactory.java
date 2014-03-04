cce9b71cbab5a823f1fa36d9dc69e6471627e092
objectundefined <gabriel.lipson@gmail.com>
2014-03-04 01:18:40 -0800

    building against neo4j 2.0.1

--- src/main/java/de/jexp/zmq/CypherServerKernelExtensionFactory.java
+++ src/main/java/de/jexp/zmq/CypherServerKernelExtensionFactory.java
@@ -2,15 +2,21 @@ package de.jexp.zmq;
 
 import org.neo4j.graphdb.GraphDatabaseService;
 import org.neo4j.graphdb.factory.Description;
-import org.neo4j.graphdb.factory.GraphDatabaseSetting;
-import org.neo4j.graphdb.factory.GraphDatabaseSettings;
 import org.neo4j.kernel.configuration.Config;
 import org.neo4j.kernel.extension.KernelExtensionFactory;
 import org.neo4j.kernel.impl.util.StringLogger;
 import org.neo4j.kernel.lifecycle.Lifecycle;
 
+
+import static org.neo4j.helpers.Settings.HOSTNAME_PORT;
+import static org.neo4j.helpers.Settings.INTEGER;
+import static org.neo4j.helpers.Settings.setting;
+
+import org.neo4j.graphdb.config.Setting;
+import org.neo4j.helpers.HostnamePort;
+
+
 import static org.neo4j.helpers.Settings.*;
-import static org.neo4j.helpers.Settings.ANY;
 
 /**
  * @author mh
@@ -20,8 +26,9 @@ public class CypherServerKernelExtensionFactory extends KernelExtensionFactory<C
 
     @Description("Settings for the Cypher Remoting Extension")
     public static abstract class CypherRemotingSettings {
-        public static GraphDatabaseSetting.HostnamePortSetting cypher_remoting_address = new GraphDatabaseSetting.HostnamePortSetting(setting("cypher_remoting_address", HOSTNAME_PORT, ":5555" )); // todo illegalmessage
-        public static GraphDatabaseSetting.IntegerSetting cypher_remoting_threads = new GraphDatabaseSetting.IntegerSetting(setting("cypher_remoting_threads", INTEGER, "1", illegalValueMessage("must be a thread number between 1 and 10",range(1,10))));
+
+        public static Setting<HostnamePort> cypher_remoting_address = setting( "cypher_remoting_address", HOSTNAME_PORT, ":5555" );
+        public static Setting<Integer> cypher_remoting_threads = setting( "cypher_remoting_threads", INTEGER, "1", illegalValueMessage("must be a thread number between 1 and 10",range(1,10)));
     }
 
     public CypherServerKernelExtensionFactory() {
